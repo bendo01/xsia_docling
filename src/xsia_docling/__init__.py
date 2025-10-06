8934d271220a7bdb567613bba59b3c3733c6957a
Benny L.E.P <bendo01@gmail.com>
2025-10-06 10:45:11 +0800

    fix

--- src/xsia_docling/__init__.py
+++ src/xsia_docling/__init__.py
@@ -1,2 +1,8 @@
-def main() -> None:
-    print("Hello from xsia-docling!")
+from docling.document_converter import DocumentConverter
+
+
+def main() -> None: # pyright: ignore[reportUnknownParameterType]
+    converter = DocumentConverter()
+    source = "https://arxiv.org/pdf/2408.09869"  # file path or URL
+    doc = converter.convert(source).document
+    print(doc.export_to_markdown())  # output: "### Docling Technical Report[...]"
